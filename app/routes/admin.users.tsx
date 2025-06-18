92d43e35f156db2d72ed52ce6a9673e099b7fc7d
Gyan-Mends <gyankwadwomends2001@gmail.com>
2025-06-17 14:36:24 -1200

    fixes

--- app/routes/admin.users.tsx
+++ app/routes/admin.users.tsx
@@ -636,7 +636,7 @@ const Users = () => {
                                 />
                             ) : (
                                 <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-                                    <FileUploader className="h-20 w-20 !text-gray-600" />
+                                    <FileUpload className="h-20 w-20 !text-gray-600" />
                                 </span>
                             )}
                         </div>
